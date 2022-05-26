4418fce0a13709559883b18f4484a1b8b3bced2a
Sadeepi Samaranayake <86165230+Sadeepi@users.noreply.github.com>
2022-05-26 06:53:18 +0530

    Create libraries.R
    libraries

---
+++ libraries.R
@@ -0,0 +1,23 @@
+#libraries are group of functions which are available for performing some specific area tasks
+library(dplyr)    
+require(dplyr)                    # library or require both can use
+library(randomForest)
+install.packages("randomFotest")
+#remove.packages("agasfahd")      # to uninstall a package
+
+# Inbuilt data sets
+data("iris")
+head(iris)
+tail(iris)
+data("airmiles")
+head(airmiles)
+tail(airmiles)
+AirPassengers
+data("Titanic")
+Titanic
+
+library(MASS)      # Import MASS library
+
+data("Boston")     # After importing MASS library Boston data set can import
+head(Boston)
+summary(Boston)
